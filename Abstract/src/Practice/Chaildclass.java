b07d3a3f754bb08fc63d9553d1ae28680a0218a5
SR974 <vasus3869@gmail.com>
2023-09-26 15:05:32 +0530

    Read the abstraction code

---
+++ Abstract/src/Practice/Chaildclass.java
@@ -0,0 +1,34 @@
+package Practice;
+
+
+public class Chaildclass extends Parentcalss {
+	
+	
+
+	@Override
+	public void srinu() {
+		System.out.println("my name is srinu");
+		
+	}
+
+	@Override
+	public void srvan() {
+	 System.out.println("my name is sravan");
+		
+	}
+
+	@Override
+	public void vasu() {
+		System.out.println("my name is vasu");
+		
+	}
+	public static void main(String[] args) {
+		Chaildclass d = new Chaildclass();
+		d.geethika();
+		d.gowthami();
+		d.srinu();
+		d.srvan();
+		d.vasu();
+		
+	}
+}
---
+++ Abstract/src/Practice/Parentcalss.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public abstract class Parentcalss {
+	public abstract void srinu();
+	public abstract void srvan();
+	public abstract void vasu();
+	public void geethika() {
+		System.out.println("my name is geethika");
+	}
+     public void gowthami() {
+    	 System.out.println("my name is gowthami");
+     }
+}
+//1 an abstract method and class should contain abstract keyword
+
+//2 it should contain semicolon
+	// 3 abstarct method never contains any method body but in normal method we can write the body 
+	// 4 in abstract class we can't create object for class but we can inherit that abstract class
+	// 5 it is a partial abstraction
+	// 6 if at least one method is an abstract class is also abstract 
+// 7 in abstarct class we can write normal methods and static methods from java 1.8 version but in normal class we can't create abstract methods
